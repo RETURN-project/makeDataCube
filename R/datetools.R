cf70b3f6824db741fb7dc937f51543e347c7dba5
Pablo Rodríguez-Sánchez <pablo.rodriguez.sanchez@gmail.com>
2021-03-16 15:34:19 +0100

    Add partition_dates

--- R/datetools.R
+++ R/datetools.R
@@ -34,3 +34,38 @@ date_to_vec <- function(date) {
 
   return(vec)
 }
+
+#' Given a start and an endtime, creates n partitions of equal or similar sizes
+#'
+#' @param starttime Start time, as a vector
+#' @param endtime End time, as a vector
+#' @param n Number of desired partitions
+#'
+#' @return A vector of intervals containing all partitions
+#' @export
+partition_dates <- function(starttime, endtime, n) {
+  # Measure the input data
+  start_date <- vec_to_date(starttime)
+  end_date <- vec_to_date(endtime)
+  n_days <- as.integer(end_date - start_date)
+
+  # Number of days per partition
+  # Notice that often it is not possible to make n partitions of equal sizes
+  # (e.g.: 11 days in 3 partitions cannot be partitioned as 3,3,3 nor as 4,4,4).
+  # This is why we have to use integer division here.
+  n_days_par <- n_days %/% n
+
+  # Create and populate the partitions
+  timespan <- interval()
+  for(i in 1:n) {
+    # Each element of time span represents a subinterval
+    timespan[i] <- interval(start_date + n_days_par * (i - 1),
+                            start_date + n_days_par * i)
+  }
+
+  # Override the last interval to make sure it goes all the way to the end
+  # in case of integer division problems
+  int_end(timespan[n]) <- end_date
+
+  return(timespan)
+}
--- tests/testthat/test-datetools.R
+++ tests/testthat/test-datetools.R
@@ -1,4 +1,4 @@
-context("Date time auxiliary functions")
+context("Date time parsers")
 
 test_that("Vector to date", {
   # Check that the parsing is properly done
@@ -19,4 +19,45 @@ test_that("Inversion", {
   d <- date("2021-3-16")
   identical <- (vec_to_date(date_to_vec(d)) == d)
   expect_true(identical)
+})
+
+context("Date partition")
+
+test_that("Controlled example", {
+  starttime <- c(2021, 1, 1)
+  endtime <- c(2021, 1, 13)
+
+  partition <- partition_dates(starttime, endtime, n = 2)
+
+  # Check the partition manually
+  expected <- c(interval(vec_to_date(starttime), vec_to_date(c(2021, 1, 7))),
+                interval(vec_to_date(c(2021, 1, 7)), vec_to_date(endtime)))
+
+  expect_equal(partition, expected)
+})
+
+test_that("Integer / noninteger division", {
+  starttime <- c(1999, 12, 1)
+  endtime <- c(2021, 3, 16) # Time difference of 7776 days
+
+  # 7776 is divisible by 8 ...
+  partition_even <- partition_dates(starttime, endtime, n = 8)
+  # ... so we expect 8 subpartitions ...
+  expect_equal(length(partition_even), 8)
+  # ... all of them equal ...
+  lengths <- int_length(partition_even)
+  expect_true(all(lengths == max(lengths)))
+  # ... and adding up to the total, initial interval
+  expect_equal(sum(partition_even), 7776 * 3600 * 24) # In seconds
+
+  # 7776 is not divisible by 7 ...
+  partition_uneven <- partition_dates(starttime, endtime, n = 7)
+  # ... so we expect 8 subpartitions ...
+  expect_equal(length(partition_uneven), 7)
+  # ... the last of them, larger than the rest ...
+  lengths <- int_length(partition_uneven)
+  expect_true(all(lengths[1:6] == max(lengths[1:6])))
+  expect_true(lengths[7] > lengths[1])
+  # ... and adding up to the total, initial interval
+  expect_equal(sum(partition_uneven), 7776 * 3600 * 24) # In seconds
 })
